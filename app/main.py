888afaeec3d9edae989a2bb41f3fb3f8b3a4487c
GabrielGalante1 <galante.biel@gmail.com>
2025-04-22 16:42:54 -0300

    estruturacao

--- app/main.py
+++ app/main.py
@@ -7,6 +7,6 @@ app = FastAPI()
 def read_hello():
     return {'Hello': 'World'}
 
-@app.get('/items/{item_id}')
+@app.get('/serie/{item_id}')
 def read_item(item_id: int, query: Union[str, None] = None):
     return {'item_id': item_id, 'query': query}
